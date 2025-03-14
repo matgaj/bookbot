fd1222792444ea45e64269b090f7ffd0566e3dea
matgaj <10777373+matgaj@users.noreply.github.com>
2025-03-14 22:44:33 +0100

    print report with sorted character occurrence

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_words, count_char_appearances
+from stats import count_words, count_char_appearances, sort_char_by_num_appearance
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -6,11 +6,25 @@ def get_book_text(filepath):
     return file_contents
 
 def main():
-    book = get_book_text("books/frankenstein.txt")
+    book_path = "books/frankenstein.txt"
+    book = get_book_text(book_path)
     num_words = count_words(book)
     char_appearances = count_char_appearances(book)
-    print(f"{num_words} words found in the document")
-    print(char_appearances)
+    # print(f"{num_words} words found in the document")
+    # print(char_appearances)
 
+    sorted_char_appearance = sort_char_by_num_appearance(char_appearances)
+    
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+
+    for k in sorted_char_appearance:
+        if k["char"].isalpha():
+            print(f"{k["char"]}: {k["num"]}")
+
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -11,3 +11,13 @@ def count_char_appearances(text):
         else:
             num_appearances[char] = 1
     return num_appearances
+
+def sort_char_by_num_appearance(char_appearances):
+    sorted_char_appearance = []
+    for k in char_appearances:
+        char_stats = {}
+        char_stats["char"] = k
+        char_stats["num"] = char_appearances[k]
+        sorted_char_appearance.append(char_stats)
+        sorted_char_appearance.sort(reverse=True, key=lambda x: x["num"])
+    return sorted_char_appearance
