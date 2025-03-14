2bbc5c152032e3faea99e17a4f36d79198249cac
matgaj <10777373+matgaj@users.noreply.github.com>
2025-03-14 22:17:28 +0100

    add word counter

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+from stats import count_words
+
 def get_book_text(filepath):
     with open(filepath) as f:
         file_contents = f.read()
     return file_contents
 
+
 def main():
-    print(get_book_text("books/frankenstein.txt"))
+    print(f"{count_words(get_book_text("books/frankenstein.txt"))} words found in the document")
 
 main()
