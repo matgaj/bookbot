7bbc28614802ecfa5abefd00d4bb37921fd1966f
matgaj <10777373+matgaj@users.noreply.github.com>
2025-03-14 22:50:38 +0100

    take book path as cmd arg

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import count_words, count_char_appearances, sort_char_by_num_appearance
+import sys
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -6,7 +7,12 @@ def get_book_text(filepath):
     return file_contents
 
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    
+    book_path = sys.argv[1]
+
     book = get_book_text(book_path)
     num_words = count_words(book)
     char_appearances = count_char_appearances(book)
