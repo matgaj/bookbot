52d14aa616432f6c37e291fa51c85ca89f7a6d6c
matgaj <10777373+matgaj@users.noreply.github.com>
2025-03-14 22:26:41 +0100

    add character occurence counting

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-from stats import count_words
+from stats import count_words, count_char_appearances
 
 def get_book_text(filepath):
     with open(filepath) as f:
         file_contents = f.read()
     return file_contents
 
-
 def main():
-    print(f"{count_words(get_book_text("books/frankenstein.txt"))} words found in the document")
+    book = get_book_text("books/frankenstein.txt")
+    num_words = count_words(book)
+    char_appearances = count_char_appearances(book)
+    print(f"{num_words} words found in the document")
+    print(char_appearances)
+
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,13 @@
 def count_words(text):
     words = text.split()
     return len(words)
+
+def count_char_appearances(text):
+    text = text.lower()
+    num_appearances = {}
+    for char in text:
+        if char in num_appearances:
+            num_appearances[char] += 1
+        else:
+            num_appearances[char] = 1
+    return num_appearances
